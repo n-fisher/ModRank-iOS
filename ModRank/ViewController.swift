d2287323133af6b5ac14e3acbf1f79f88609ef42
Nick Fisher <nickf@ifixit.com>
2017-11-04 16:38:58 -0700

    Initial Commit

---
+++ ModRank/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ModRank
+//
+//  Created by Nick Fisher on 11/4/17.
+//  Copyright © 2017 Nick Fisher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
