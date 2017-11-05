43e9f79253bc93a4995ecc1fa4c7e99792889eba
Nick Fisher <nickf@ifixit.com>
2017-11-05 10:17:10 -0800

    Add Data Model
    - extending tutorial from here on out

---
+++ ModRank/ModInfo.swift
@@ -0,0 +1,60 @@
+//
+//  ModInfo.swift
+//  ModRank
+//
+//  Created by Nick Fisher on 11/5/17.
+//  Copyright © 2017 Nick Fisher. All rights reserved.
+//
+
+import UIKit
+
+class ModInfo {
+    
+    //MARK: Properties
+    var title: String
+    var id: Int
+    var itemTitle: String
+    var comments: Int
+    var subs: Int
+    var favs: Int
+    var views: Int
+    var unsubscribes: Int
+    var img: String
+    var favsRank: Int
+    var favsPercent: Float
+    var subsRank: Int
+    var subsPercent: Float
+    var unsubscribesRank: Int
+    var unsubscribesPercent: Float
+    var viewsRank: Int
+    var viewsPercent: Float
+    var commentsRank: Int
+    var commentsPercent: Float
+    
+    //MARK: Initialization
+    init(title: String, id: Int, itemTitle: String, comments: Int, subs: Int,
+         favs: Int, views: Int, unsubscribes: Int, img: String, favsRank: Int,
+         favsPercent: Float, subsRank: Int, subsPercent: Float, unsubscribesRank: Int,
+         unsubscribesPercent: Float, viewsRank: Int, viewsPercent: Float,
+         commentsRank: Int, commentsPercent: Float) {
+        self.title = title;
+        self.id = id;
+        self.itemTitle = title;
+        self.comments = comments;
+        self.subs = subs;
+        self.favs = favs;
+        self.views = views;
+        self.unsubscribes = unsubscribes;
+        self.img = img;
+        self.favsRank = favsRank;
+        self.favsPercent = favsPercent;
+        self.subsRank = subsRank;
+        self.subsPercent = subsPercent;
+        self.unsubscribesRank = unsubscribesRank;
+        self.unsubscribesPercent = unsubscribesPercent;
+        self.viewsRank = viewsRank;
+        self.viewsPercent = viewsPercent;
+        self.commentsRank = commentsRank;
+        self.commentsPercent = commentsPercent;
+    }
+}
--- ModRank/ViewController.swift
+++ ModRank/ViewController.swift
@@ -8,14 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     //MARK: Properties
     @IBOutlet weak var modIDField: UITextField!
     @IBOutlet weak var modIDLabel: UILabel!
+    @IBOutlet weak var modImage: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        modIDField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,7 +25,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    //MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        modIDLabel.text = textField.text
+    }
+    
     //MARK: Actions
+    @IBAction func changeImage(_ sender: UITapGestureRecognizer) {
+        modIDField.resignFirstResponder()
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .photoLibrary
+        // Make sure ViewController is notified when the user picks an image.
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
+    }
     @IBAction func addModID(_ sender: UIButton) {
         modIDLabel.text = modIDField.text
     }
